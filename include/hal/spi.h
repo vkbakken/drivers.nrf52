a02279e427b115f1ac30b153403f833ddc2c270d
Unknown <liemle@mpire.vn>
2018-12-14 18:10:58 +0700

    update SPI api proposal

--- include/hal/spi.h
+++ include/hal/spi.h
@@ -19,8 +19,9 @@ typedef struct hal_spi_instance hal_spi_instance_t;
 
 void hal_spi_init(void);
 void hal_spi_deinit(void);
-bool hal_spi_write(hal_spi_instance_t *spi_instance, uint8_t reg_addr, uint8_t *data, uint8_t size);
-bool hal_spi_read(hal_spi_instance_t *spi_instance, uint8_t reg_addr, uint8_t *data, uint8_t size);
+
+bool hal_spi_write(hal_spi_instance_t *spi_instance, uint8_t *data, uint8_t size);
+bool hal_spi_read(hal_spi_instance_t *spi_instance, uint8_t *data, uint8_t size);
 
 #ifdef __cplusplus
 }
