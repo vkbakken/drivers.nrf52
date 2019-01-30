1a9d0ea58ec4b6f867b448252d672d955c451ea4
Unknown <liemle@mpire.vn>
2019-01-30 18:21:22 +0700

    update gpio hal proposal

--- include/hal/hal_gpio.h
+++ include/hal/hal_gpio.h
@@ -31,45 +31,104 @@ typedef enum {
 /**
  * @brief 
  * 
+ * @param port 
  * @param pin 
  * @param dir 
  * @param pull 
  */
-void hal_gpio_config(uint8_t pin, gpio_dir_t dir, gpio_pull_t pull);
+void hal_gpio_config(uint32_t * port, uint8_t pin, gpio_dir_t dir, gpio_pull_t pull);
 
 /**
  * @brief 
  * 
+ * @param port 
  * @param pin 
  * @return true 
  * @return false 
  */
-bool hal_gpio_input_read(uint8_t pin);
+bool hal_gpio_pin_read(uint32_t * port, uint8_t pin);
 
 /**
  * @brief 
  * 
+ * @param port 
  * @param pin 
  * @param value 
  */
-void hal_gpio_output_write(uint8_t pin, bool value);
+void hal_gpio_pin_set(uint32_t * port, uint8_t pin, bool value);
 
 /**
  * @brief 
  * 
+ * @param port 
+ * @param pin 
+ * @param value 
+ */
+void hal_gpio_pin_clear(uint32_t * port, uint8_t pin, bool value);
+
+/**
+ * @brief 
+ * 
+ * @param port 
  * @param pin 
  */
-void hal_gpio_output_toggle(uint8_t pin);
+void hal_gpio_pin_toggle(uint32_t * port, uint8_t pin);
 
 /**
  * @brief 
  * 
+ * @param port 
+ * @param pin 
+ * @return uint32_t 
+ */
+uint32_t hal_gpio_port_read(uint32_t * port, uint8_t pin);
+
+/**
+ * @brief 
+ * 
+ * @param port 
+ * @param value 
+ */
+void hal_gpio_port_set(uint32_t * port, bool value);
+
+/**
+ * @brief 
+ * 
+ * @param port 
+ * @param value 
+ */
+void hal_gpio_port_clear(uint32_t * port, bool value);
+
+/**
+ * @brief 
+ * 
+ * @param port 
+ */
+void hal_gpio_port_toggle(uint32_t * port);
+
+/**
+ * @brief 
+ * 
+ * @param port 
  * @param pin 
  * @param interrupt_signal 
  * @param callback 
+ * @return true 
+ * @return false 
  */
-bool hal_gpio_attach_interrupt(uint8_t pin, gpio_interrupt_signal_t interrupt_signal, void (*callback)(void));
+bool hal_gpio_install_interrupt(uint32_t * port, uint8_t pin, gpio_interrupt_signal_t interrupt_signal, void (*callback)(void));
 
+/**
+ * @brief 
+ * 
+ * @param port 
+ * @param pin 
+ * @param interrupt_signal 
+ * @param callback 
+ * @return true 
+ * @return false 
+ */
+bool hal_gpio_uninstall_interrupt(uint32_t * port, uint8_t pin, gpio_interrupt_signal_t interrupt_signal, void (*callback)(void));
 
 #ifdef __cplusplus
 }
