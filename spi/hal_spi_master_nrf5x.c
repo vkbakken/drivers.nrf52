e8da247182cce58b194fd8c676fcff5d3003f6f4
LiemLB <liemle@mpire.vn>
2019-02-14 19:21:31 +0700

    Merge hal_spi for nrf52810 and nrf52832

--- spi/hal_spi_master_nrf5x.c
+++ spi/hal_spi_master_nrf5x.c
@@ -1,6 +1,19 @@
 #include "cpu/io.h"
 #include "hal/hal_spi_master.h"
 
+#if	defined(NRF52810_H)
+	#define SPI_REG				NRF_SPIM0
+    #define SPI_IRQ				SPIM0_SPIS0_IRQn
+    #define SPI_IRQ_HANDLER		spi0_handler
+#elif defined(NRF52_H)
+	#define SPI_REG				NRF_SPIM1
+    #define SPI_IRQ				SPIM1_SPIS1_TWIM1_TWIS1_SPI1_TWI1_IRQn
+    #define SPI_IRQ_HANDLER		serialbox1_handler
+#else
+	#error "This driver only support NRF52 and NRF52810 CPU"
+#endif
+
+
 static SemaphoreHandle_t xSemaphoreSPI = NULL;
 static StaticSemaphore_t xSemaphoreBufferSPI;
 
@@ -27,31 +40,31 @@ void hal_spi_init(hal_spi_instance_t const *const spi_instance) {
 	/* config pin IO used for SPI */
 
 	/* Bind IO pin to SPI peripheral */
-	NRF_SPIM1->PSEL.SCK = spi_instance->config.pin_CLK;
-	NRF_SPIM1->PSEL.MOSI = spi_instance->config.pin_MOSI;
-	NRF_SPIM1->PSEL.MISO = spi_instance->config.pin_MISO;
+	SPI_REG->PSEL.SCK = spi_instance->config.pin_CLK;
+	SPI_REG->PSEL.MOSI = spi_instance->config.pin_MOSI;
+	SPI_REG->PSEL.MISO = spi_instance->config.pin_MISO;
 
 	/* Config SPI peripheral */
-	NRF_SPIM1->FREQUENCY = hal_spi_nrfx_freq_convert(spi_instance->config.frequency);
-	NRF_SPIM1->CONFIG = (spi_instance->config.bit_order << SPIM_CONFIG_ORDER_Pos) |
+	SPI_REG->FREQUENCY = hal_spi_nrfx_freq_convert(spi_instance->config.frequency);
+	SPI_REG->CONFIG = (spi_instance->config.bit_order << SPIM_CONFIG_ORDER_Pos) |
 						(spi_instance->config.clock_polarity << SPIM_CONFIG_CPOL_Pos) |
 						(spi_instance->config.clock_phase << SPIM_CONFIG_CPHA_Pos);
 
-	NRF_SPIM1->ENABLE = (SPIM_ENABLE_ENABLE_Enabled << SPIM_ENABLE_ENABLE_Pos);
-	NRF_SPIM1->INTENSET = SPIM_INTENSET_END_Msk;
+	SPI_REG->ENABLE = (SPIM_ENABLE_ENABLE_Enabled << SPIM_ENABLE_ENABLE_Pos);
+	SPI_REG->INTENSET = SPIM_INTENSET_END_Msk;
 
-	NVIC_SetPriority(SPIM1_SPIS1_TWIM1_TWIS1_SPI1_TWI1_IRQn, configLIBRARY_LOWEST_INTERRUPT_PRIORITY);
-	NVIC_ClearPendingIRQ(SPIM1_SPIS1_TWIM1_TWIS1_SPI1_TWI1_IRQn);
-	NVIC_EnableIRQ(SPIM1_SPIS1_TWIM1_TWIS1_SPI1_TWI1_IRQn);
+	NVIC_SetPriority(SPI_IRQ, configLIBRARY_LOWEST_INTERRUPT_PRIORITY);
+	NVIC_ClearPendingIRQ(SPI_IRQ);
+	NVIC_EnableIRQ(SPI_IRQ);
 
 	xSemaphoreSPI = xSemaphoreCreateBinaryStatic(&xSemaphoreBufferSPI);
 	xSemaphoreGive(xSemaphoreSPI);
 }
 
 void hal_spi_deinit(hal_spi_instance_t const *const spi_instance) {
-	NRF_SPIM1->ENABLE = (SPIM_ENABLE_ENABLE_Disabled << SPIM_ENABLE_ENABLE_Pos);
+	SPI_REG->ENABLE = (SPIM_ENABLE_ENABLE_Disabled << SPIM_ENABLE_ENABLE_Pos);
 
-	NVIC_DisableIRQ(SPIM1_SPIS1_TWIM1_TWIS1_SPI1_TWI1_IRQn);
+	NVIC_DisableIRQ(SPI_IRQ);
 	vSemaphoreDelete(xSemaphoreSPI);
 }
 
@@ -59,20 +72,20 @@ bool hal_spi_wr(hal_spi_instance_t const *const spi_instance, uint8_t *data_w, u
 	bool ret = false;
 
 	if (xSemaphoreTake(xSemaphoreSPI, 0) == pdTRUE) {
-		NRF_SPIM1->INTENCLR = SPIM_INTENSET_STOPPED_Msk | SPIM_INTENSET_ENDRX_Msk |
+		SPI_REG->INTENCLR = SPIM_INTENSET_STOPPED_Msk | SPIM_INTENSET_ENDRX_Msk |
 							  SPIM_INTENSET_END_Msk | SPIM_INTENSET_ENDTX_Msk |
 							  SPIM_INTENSET_STARTED_Msk;
 		/* select a slave and start spi transaction */
 		NRF_P0->OUTCLR = 0x1UL << spi_instance->SS_pin;
 
-		NRF_SPIM1->TXD.PTR = (uint32_t)data_w;
-		NRF_SPIM1->TXD.MAXCNT = size_w;
-		NRF_SPIM1->RXD.PTR = (uint32_t)data_r;
-		NRF_SPIM1->RXD.MAXCNT = size_r;
+		SPI_REG->TXD.PTR = (uint32_t)data_w;
+		SPI_REG->TXD.MAXCNT = size_w;
+		SPI_REG->RXD.PTR = (uint32_t)data_r;
+		SPI_REG->RXD.MAXCNT = size_r;
 
-		NRF_SPIM1->EVENTS_END = 0x0UL;
-		NRF_SPIM1->INTENSET = SPIM_INTENSET_END_Msk;
-		NRF_SPIM1->TASKS_START = 0x1UL;
+		SPI_REG->EVENTS_END = 0x0UL;
+		SPI_REG->INTENSET = SPIM_INTENSET_END_Msk;
+		SPI_REG->TASKS_START = 0x1UL;
 
 		ret = xSemaphoreTake(xSemaphoreSPI, spi_instance->timeout);
 		xSemaphoreGive(xSemaphoreSPI);
@@ -91,11 +104,11 @@ bool hal_spi_read(hal_spi_instance_t const *const spi_instance, uint8_t *data, u
 	return hal_spi_wr(spi_instance, NULL, 0, data, size);
 }
 
-void serialbox1_handler(void) {
+void SPI_IRQ_HANDLER(void) {
 	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 
-	if (NRF_SPIM1->EVENTS_END) {
-		NRF_SPIM1->EVENTS_END = 0x0UL;
+	if (SPI_REG->EVENTS_END) {
+		SPI_REG->EVENTS_END = 0x0UL;
 		xSemaphoreGiveFromISR(xSemaphoreSPI, &xHigherPriorityTaskWoken);
 	}
 
