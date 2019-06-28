6fc67ed5b5ddfff5dee3e580923e2a1fe2678e86
liemleba <liemle@mpire.vn>
2019-06-28 13:08:30 +0700

    update hal_rtc module

--- rtc/hal_rtc_nrf5x.c
+++ rtc/hal_rtc_nrf5x.c
@@ -8,12 +8,14 @@
 void (*internal_timeout_func)(void);
 void (*internal_overflow_func)(void);
 
-void hal_rtc_init(void) {
+void hal_rtc_init(void)
+{
 	/* Start the clock source if not already started*/
 	if ((NRF_CLOCK->LFCLKSTAT & CLOCK_LFCLKSTAT_STATE_Msk) == CLOCK_LFCLKSTAT_STATE_NotRunning) {
 		NRF_CLOCK->LFCLKSRC = CLOCK_LFCLKSRC_SRC_RC << CLOCK_LFCLKSRC_SRC_Pos;
 		NRF_CLOCK->TASKS_LFCLKSTART = 1;
-		while (NRF_CLOCK->EVENTS_LFCLKSTARTED == 0);
+		while (NRF_CLOCK->EVENTS_LFCLKSTARTED == 0)
+			;
 		NRF_CLOCK->EVENTS_LFCLKSTARTED = 0;
 	}
 
@@ -29,7 +31,8 @@ void hal_rtc_init(void) {
 	NVIC_EnableIRQ(RTC0_IRQn);
 }
 
-void hal_rtc_deinit(void) {
+void hal_rtc_deinit(void)
+{
 	NRF_RTC0->TASKS_STOP = 1;
 	NVIC_DisableIRQ(RTC0_IRQn);
 	NVIC_ClearPendingIRQ(RTC0_IRQn);
@@ -37,16 +40,19 @@ void hal_rtc_deinit(void) {
 	NRF_CLOCK->TASKS_LFCLKSTOP = 1;
 }
 
-uint32_t hal_rtc_get_tick(void) {
+uint32_t hal_rtc_get_tick(void)
+{
 	return NRF_RTC0->COUNTER;
 }
 
-void hal_rtc_clear_tick(void){
+void hal_rtc_clear_tick(void)
+{
 	NRF_RTC0->TASKS_CLEAR;
 }
 
-bool hal_rtc_set_periodic(void (*timeout_func)(void), uint32_t ticks) {
-	if(ticks & 0x00FFFFFF != 0){
+bool hal_rtc_set_periodic(void (*timeout_func)(void), uint32_t ticks)
+{
+	if (ticks & 0x00FFFFFF != 0) {
 		return false;
 	}
 	uint32_t tick;
@@ -54,33 +60,48 @@ bool hal_rtc_set_periodic(void (*timeout_func)(void), uint32_t ticks) {
 	return hal_rtc_set_compare(timeout_func, tick);
 }
 
-bool hal_rtc_set_compare(void (*timeout_func)(void), uint32_t tick) {
-	if(NULL == timeout_func){
+bool hal_rtc_set_compare(void (*timeout_func)(void), uint32_t tick)
+{
+	if (NULL == timeout_func) {
 		return false;
 	}
 
-    internal_timeout_func = timeout_func;
-	
+	internal_timeout_func = timeout_func;
+
 	NRF_RTC0->CC[0] = 0x00FFFFFF & tick;
 	NRF_RTC0->INTENSET = RTC_INTENSET_COMPARE0_Msk;
 	return true;
 }
 
-void hal_rtc_enable_overflow(void (*overflow_func)(void)){
+void hal_rtc_enable_overflow(void (*overflow_func)(void))
+{
 	internal_overflow_func = overflow_func;
 
-    NRF_RTC0->EVENTS_OVRFLW = 0;
+	NRF_RTC0->EVENTS_OVRFLW = 0;
 	NRF_RTC0->INTENSET = RTC_INTENSET_OVRFLW_Msk;
 }
 
-void rtc0_handler(void) {
+void hal_rtc_disable_overflow(void (*overflow_func)(void))
+{
+	internal_overflow_func = NULL;
+
+	NRF_RTC0->EVENTS_OVRFLW = 0;
+	NRF_RTC0->INTENCLR = RTC_INTENSET_OVRFLW_Msk;
+}
+
+void rtc0_handler(void)
+{
 	if (NRF_RTC0->EVENTS_COMPARE[0]) {
 		NRF_RTC0->EVENTS_COMPARE[0] = 0;
-		internal_timeout_func();
+		NRF_RTC0->INTENCLR = RTC_INTENSET_COMPARE0_Msk;
+		if (internal_timeout_func)
+			internal_timeout_func();
 	}
 
-	if(NRF_RTC0->EVENTS_OVRFLW){
+	if (NRF_RTC0->EVENTS_OVRFLW) {
 		NRF_RTC0->EVENTS_OVRFLW = 0;
-        internal_overflow_func();
+		NRF_RTC0->INTENCLR = RTC_INTENSET_OVRFLW_Msk;
+		if (internal_overflow_func)
+			internal_overflow_func();
 	}
 }
